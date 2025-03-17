5d9d32206484c150cc716c0055b3024f273798d9
Isaac George <git-mail@isaacgeorge.net>
2025-03-17 12:48:38 -0500

    Start on converting to zig 0.14.0

--- build.zig
+++ build.zig
@@ -13,8 +13,10 @@ pub fn build(b: *std.Build) !void {
     const exe = b.addExecutable(.{
         .name = "website",
         .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = b.createModule(.{
+            .target = target,
+            .optimize = optimize,
+        }),
     });
     exe.root_module.addImport("zap", zap.module("zap"));
     b.installArtifact(exe);
@@ -124,8 +126,8 @@ const PrintOutputLocation = struct {
         return print;
     }
 
-    fn make(step: *std.Build.Step, prog_node: std.Progress.Node) !void {
-        _ = prog_node;
+    fn make(step: *std.Build.Step, options: std.Build.Step.MakeOptions) anyerror!void {
+        _ = options;
 
         const print: *PrintOutputLocation = @fieldParentPtr("step", step);
 
